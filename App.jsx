e3e8a6240ca0d81c8b5ed17b03cc7d305e9a0126
daralhikmah366-cpu <daralhikmah366@gmail.com>
2025-09-23 19:31:32 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 
-// Import all page components
+// Import page components
 import Home from './Pages/Home';
 import About from './Pages/About';
-import Programs from './Pages/Programs';
-import Events from './Pages/Events';
-import Donate from './Pages/Donate';
-import Articles from './Pages/Articles';
 import Contact from './Pages/Contact';
+import Donate from './Pages/Donate';
 
 function App() {
   return (
@@ -18,11 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/programs" element={<Programs />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/donate" element={<Donate />} />
-          <Route path="/articles" element={<Articles />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/donate" element={<Donate />} />
         </Routes>
       </Layout>
     </Router>
